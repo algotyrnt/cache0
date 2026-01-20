8ffe2817d63be84453e5f2cf79b38a14da55e744
punjitha <132387971+algotyrnt@users.noreply.github.com>
2026-01-20 10:55:06 +0530

    Add destructor and file I/O methods for Cache0 class

--- src/cache0.cpp
+++ src/cache0.cpp
@@ -5,7 +5,16 @@
 //  Created by Punjitha Bandara on 2026-01-19.
 //
 
+#include <iostream>
 #include "../include/cache0.hpp"
+
+Cache0::Cache0() {
+    load();
+}
+
+Cache0::~Cache0() {
+    save();
+}
 
 void Cache0::put(const std::string& key, const std::string& value) {
     store[key] = value;
@@ -20,4 +29,37 @@ std::optional<std::string> Cache0::get(const std::string& key) {
 
 bool Cache0::remove(const std::string& key) {
     return store.erase(key) > 0;
+}
+
+void Cache0::save() {
+    std::ofstream file(filename);
+    
+    if (file.is_open()) {
+        for (const auto& pair : store) {
+            file << pair.first << "=" << pair.second << std::endl;
+        }
+        file.close();
+        std::cout << "[cache0] Data saved to disk." << std::endl;
+    } else {
+        std::cerr << "[cache0] Error: Could not save data." << std::endl;
+    }
+}
+
+void Cache0::load() {
+    std::ifstream file(filename);
+    std::string line;
+
+    if (file.is_open()) {
+        while (std::getline(file, line)) {
+            size_t delimiterPos = line.find('=');
+            
+            if (delimiterPos != std::string::npos) {
+                std::string key = line.substr(0, delimiterPos);
+                std::string value = line.substr(delimiterPos + 1);
+                store[key] = value;
+            }
+        }
+        file.close();
+        std::cout << "[cache0] Data loaded." << std::endl;
+    }
 }
